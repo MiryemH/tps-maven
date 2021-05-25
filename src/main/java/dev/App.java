5bc9792521d35ebf9c4afd02d15fd2b904932523
Miryem HRARTI <hrartim@yahoo.fr>
2021-05-25 12:45:59 +0200

    Etape2 du TP01

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String maChaine = "Hello Miryem";
+        System.out.println(maChaine);
     }
 }
